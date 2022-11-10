ce5b4d55589d47c2431518412c358b6a6ab72a77
EricchiYukia <git@ericchi.moe>
2022-11-10 17:06:48 +0100

    Qualche ottimizzazione

--- script.js
+++ script.js
@@ -5,15 +5,13 @@ function chiketto() {
     // Scusami Bryan. Volevo smentire il dogma delle variabili che vanno
     // scritte unicamente con caratteri dell'alfabeto latino. Non lo faccio più.
 
-    if(才 < 18){
+    if (才 < 18){
         価格 = 価格-(価格 / 100 * 20);
-    }
-
-    if(才 >= 65){
+    } else if (才 >= 65){
         価格 = 価格-(価格 / 100 * 40);
     }
 
     価格 = 価格.toFixed(2);
-    document.getElementById("kotae").innerHTML = 価格;
+    document.getElementById("kotae").innerHTML = 価格 + "€";
 }
 
