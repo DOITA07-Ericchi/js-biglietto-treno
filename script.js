f763f8b863d83d49d9369ccd476a414fc712a0db
EricchiYukia <git@ericchi.moe>
2022-11-10 17:03:31 +0100

    Finito.

---
+++ script.js
@@ -0,0 +1,19 @@
+function chiketto() {
+    let キロ = document.getElementById("kiro").value;
+    let 才 = document.getElementById("sai").value;
+    let 価格 = キロ * 0.21;
+    // Scusami Bryan. Volevo smentire il dogma delle variabili che vanno
+    // scritte unicamente con caratteri dell'alfabeto latino. Non lo faccio più.
+
+    if(才 < 18){
+        価格 = 価格-(価格 / 100 * 20);
+    }
+
+    if(才 >= 65){
+        価格 = 価格-(価格 / 100 * 40);
+    }
+
+    価格 = 価格.toFixed(2);
+    document.getElementById("kotae").innerHTML = 価格;
+}
+
